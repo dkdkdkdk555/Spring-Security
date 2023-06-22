e48dbc5da45b334257b6ec60d06100b29e207ef3
dkdkdkdk555 <dkdkdkdk555@naver.com>
2023-06-22 09:32:01 +0900

    필터 config 설정 및 jwt 필터 등록

---
+++ src/main/java/com/example/secure/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.example.secure.config;
+
+import com.example.secure.filter.MyFilter1;
+import com.example.secure.filter.MyFilter3;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.context.SecurityContextPersistenceFilter;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CorsFilter corsFilter;
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.addFilterBefore(new MyFilter3(), SecurityContextPersistenceFilter.class);
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용 않겠다.
+                .and()
+                .addFilter(corsFilter) // 크로스 오리진 무시하고 모든 요청 받는다.
+                .formLogin().disable() // 폼로그인 안쓴다.
+                .httpBasic().disable() //
+                .authorizeRequests()
+                .antMatchers("/api/v1/user/**")
+                .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/manager/**")
+                .access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/admin/**")
+                .access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll();
+
+    }
+}
---
+++ src/main/java/com/example/secure/filter/MyFilter3.java
@@ -0,0 +1,38 @@
+package com.example.secure.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class MyFilter3 implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        System.out.println("필터3");
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        /*
+           id, pw 들어와서 정상적으로 로그인 되면 토큰 만들어주고 그걸 응답해줌
+           요청할 때 마다 header 에 Authorization 에 value 값으로 토큰을 가지고 오면
+           이 토큰이 내가 만든(서버가) 토큰이 맞는지 검증만 하면 됨. ( RSA, HS256 으로)
+         */
+        if(req.getMethod().equals("POST")) {
+            System.out.println("POST 요청됨");
+            String headerAuth = req.getHeader("Authorization");
+            System.out.println(headerAuth);
+            System.out.println("필터1");
+
+            if("cors".equals(headerAuth)){
+                chain.doFilter(req, res);
+            } else {
+                PrintWriter out = res.getWriter();
+                out.println("인증안됨");
+            }
+        }
+        chain.doFilter(request, response); // 필터 체인에 등록
+    }
+}
